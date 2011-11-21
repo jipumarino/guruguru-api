9b7cb16d96c4db24e9c9ceb1ef8481f4104cff66
Juan Ignacio Pumarino <jipumarino@gmail.com>
2011-11-21 16:48:28 -0300

    Compact 2

--- guruguru.rb
+++ guruguru.rb
@@ -3,5 +3,5 @@ require 'nokogiri'
 require 'net/http'
 
 exit if ARGV.empty?
-res = Net::HTTP.post_form(URI.parse('http://pangui.heroku.com/'),{'msg' => ARGV.join(" ")})
+res = Net::HTTP.post_form URI.parse('http://pangui.heroku.com/'), 'msg' => ARGV.join(" ")
 puts Nokogiri::HTML(res.body).css('h1')[1].inner_text.gsub(/^\s+/,'').gsub(/\s+$/,'')
